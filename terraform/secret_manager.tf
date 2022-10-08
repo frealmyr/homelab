50367e8f9c0abbc2fa8883f1e9d29991baf8abc7
Fredrick Myrvoll <fredrickmyrvoll@gmail.com>
2022-10-08 18:21:56 +0200

    add external-secrets

--- terraform/secret_manager.tf
+++ terraform/secret_manager.tf
@@ -29,6 +29,7 @@ resource "google_project_iam_member" "secret_manager_accessor" {
 resource "kubernetes_secret" "secret_manager_accessor" {
   metadata {
     name = "secret-manager-accessor"
+    namespace = kubernetes_namespace.external_secrets.metadata[0].name
   }
   data = {
     "credentials.json" = base64decode(google_service_account_key.secret_manager_accessor.private_key)
